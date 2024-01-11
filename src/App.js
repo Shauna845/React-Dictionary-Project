dd9ffcc8114d129d87754a4bee3da324f2c5a05f
Shauna845 <shauna10@hotmail.co.uk>
2024-01-11 22:01:28 +0000

    Set up basic search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import logo from "./logo.jpeg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" img-fluid alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" img-fluid alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          {" "}
+          <small>
+            Coded by Shauna Bratten using{" "}
+            <a
+              href="https://github.com/Shauna845/React-Dictionary-Project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://sb-react-dictionary-app.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
